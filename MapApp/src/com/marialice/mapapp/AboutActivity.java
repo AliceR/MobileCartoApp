c78e153c258fde826820ac30e76e4a77ec69577e
AliceR <alice.ruehl@gmail.com>
2014-01-05 20:27:05 +0100

    some comments

--- MapApp/src/com/marialice/mapapp/AboutActivity.java
+++ MapApp/src/com/marialice/mapapp/AboutActivity.java
@@ -16,9 +16,7 @@ public class AboutActivity extends Activity {
 		setupActionBar();
 	}
 
-	/**
-	 * Set up the {@link android.app.ActionBar}.
-	 */
+	// Set up the {@link android.app.ActionBar}.
 	private void setupActionBar() {
 
 		getActionBar().setDisplayHomeAsUpEnabled(true);
@@ -28,7 +26,7 @@ public class AboutActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu, menu);
+		getMenuInflater().inflate(R.menu.map, menu);
 		return true;
 	}
 
--- MapApp/src/com/marialice/mapapp/MapActivity.java
+++ MapApp/src/com/marialice/mapapp/MapActivity.java
@@ -8,75 +8,95 @@ import com.google.android.gms.maps.model.TileOverlayOptions;
 import com.google.android.gms.maps.model.TileProvider;
 import com.google.android.gms.maps.model.UrlTileProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Locale;
 
-/**
- * This demonstrates how to add a tile overlay to a map.
- */
-
-
 public class MapActivity extends FragmentActivity {
 
-    /** This returns moon tiles. */
-    private static final String MAPBOX_BASEMAP_URL_FORMAT =
-            "http://api.tiles.mapbox.com/v3/maridani.go26lm2h/%d/%d/%d.png";
+	private static final String MAPBOX_BASEMAP_URL_FORMAT = "http://api.tiles.mapbox.com/v3/maridani.go26lm2h/%d/%d/%d.png";
 
-    private GoogleMap mMap;
+	private GoogleMap mMap;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
-        setUpMapIfNeeded();
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_map);
+		setUpMapIfNeeded();
+	}
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        setUpMapIfNeeded();
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu items for use in the action bar
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.map, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
 
-    private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
-            }
-        }
-    }
+	@Override
+	protected void onResume() {
+		super.onResume();
+		setUpMapIfNeeded();
+	}
 
-    private void setUpMap() {
-        mMap.setMapType(GoogleMap.MAP_TYPE_NONE);
-        
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(48.8108425,
-				14.3144269), 15));
-        
-       
-        TileProvider tileProvider = new UrlTileProvider(256, 256) {
-            @Override
-            public synchronized URL getTileUrl(int x, int y, int zoom) {
-                String s = String.format(Locale.US, MAPBOX_BASEMAP_URL_FORMAT, zoom, x, y);
-                URL url = null;
-                try {
-                    url = new URL(s);
-                } catch (MalformedURLException e) {
-                    throw new AssertionError(e);
-                }
-                return url;
-            }
-        };
+	private void setUpMapIfNeeded() {
+		// Do a null check to confirm that we have not already instantiated the
+		// map.
+		if (mMap == null) {
+			// Try to obtain the map from the SupportMapFragment.
+			mMap = ((SupportMapFragment) getSupportFragmentManager()
+					.findFragmentById(R.id.map)).getMap();
+			// Check if we were successful in obtaining the map.
+			if (mMap != null) {
+				setUpMap();
+			}
+		}
+	}
 
-        mMap.addTileOverlay(new TileOverlayOptions().tileProvider(tileProvider));
-        
-        
-    }
+	private void setUpMap() {
+		mMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+
+		mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(
+				48.8108425, 14.3144269), 15));
+
+		TileProvider tileProvider = new UrlTileProvider(256, 256) {
+			@Override
+			public synchronized URL getTileUrl(int x, int y, int zoom) {
+				String s = String.format(Locale.US, MAPBOX_BASEMAP_URL_FORMAT,
+						zoom, x, y);
+				URL url = null;
+				try {
+					url = new URL(s);
+				} catch (MalformedURLException e) {
+					throw new AssertionError(e);
+				}
+				return url;
+			}
+		};
+
+		mMap.addTileOverlay(new TileOverlayOptions().tileProvider(tileProvider));
+
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle presses on the action bar items
+		switch (item.getItemId()) {
+		case R.id.action_search:
+			return true;
+		case R.id.goto_places:
+			Intent intent = new Intent(MapActivity.this, PlacesActivity.class);
+			startActivity(intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
 }
