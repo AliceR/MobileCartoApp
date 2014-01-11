4199237ef7c26c3f2252d4dc02ab2481a0ec1b11
AliceR <alice.ruehl@gmail.com>
2014-01-11 17:00:09 +0100

    deleted comment lines

--- MapApp/src/com/marialice/mapapp/AboutActivity.java
+++ MapApp/src/com/marialice/mapapp/AboutActivity.java
@@ -12,15 +12,12 @@ public class AboutActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
-		// Show the Up button in the action bar.
 		setupActionBar();
 	}
 
-	// Set up the {@link android.app.ActionBar}.
 	private void setupActionBar() {
-
+		// Show the Up button in the action bar.
 		getActionBar().setDisplayHomeAsUpEnabled(true);
-
 	}
 
 	@Override
@@ -34,13 +31,6 @@ public class AboutActivity extends Activity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
 			NavUtils.navigateUpFromSameTask(this);
 			return true;
 		}
