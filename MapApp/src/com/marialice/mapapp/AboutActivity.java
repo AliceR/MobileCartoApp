080999adcd61189170cfdbb4c300faee4f14c539
MariDani <danielova.mariana@gmail.com>
2014-01-14 17:04:05 +0100

    about info text

--- MapApp/src/com/marialice/mapapp/AboutActivity.java
+++ MapApp/src/com/marialice/mapapp/AboutActivity.java
@@ -3,7 +3,10 @@ package com.marialice.mapapp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class AboutActivity extends Activity {
 
@@ -12,11 +15,31 @@ public class AboutActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
 		setupActionBar();
+		createLinks();		
 	}
 
 	private void setupActionBar() {
 		// Show the Up button in the action bar
 		getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+	
+	
+	public void createLinks(){
+		//create links in text views
+		((TextView) findViewById(R.id.link_useit)).setMovementMethod(LinkMovementMethod.getInstance());
+		((TextView) findViewById(R.id.link_useit)).setText(Html.fromHtml(getResources().getString(R.string.link_useit)));
+		
+		((TextView) findViewById(R.id.contact_mail)).setMovementMethod(LinkMovementMethod.getInstance());
+		((TextView) findViewById(R.id.contact_mail)).setText(Html.fromHtml(getResources().getString(R.string.contact_mail)));
+		
+		((TextView) findViewById(R.id.link_cbuseit)).setMovementMethod(LinkMovementMethod.getInstance());
+		((TextView) findViewById(R.id.link_cbuseit)).setText(Html.fromHtml(getResources().getString(R.string.link_cbuseit)));
+		
+		((TextView) findViewById(R.id.link_fbuseit)).setMovementMethod(LinkMovementMethod.getInstance());
+		((TextView) findViewById(R.id.link_fbuseit)).setText(Html.fromHtml(getResources().getString(R.string.link_fbuseit)));
+		
+		((TextView) findViewById(R.id.link_fbcbck)).setMovementMethod(LinkMovementMethod.getInstance());
+		((TextView) findViewById(R.id.link_fbcbck)).setText(Html.fromHtml(getResources().getString(R.string.link_fbcbck)));
 	}
 
 /*	@Override
