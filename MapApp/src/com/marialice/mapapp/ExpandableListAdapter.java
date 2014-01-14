4f92dc87240b95842b6de4a132e7ccd64b83ac76
AliceR <alice.ruehl@gmail.com>
2014-01-14 17:42:41 +0100

    trying to fix expandable list

--- MapApp/src/com/marialice/mapapp/ExpandableListAdapter.java
+++ MapApp/src/com/marialice/mapapp/ExpandableListAdapter.java
@@ -4,10 +4,12 @@ import java.util.HashMap;
 import java.util.List;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ExpandableListAdapter extends BaseExpandableListAdapter {
@@ -33,44 +35,38 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
 	@Override
 	public long getChildId(int groupPosition, int childPosition) {
 		return childPosition;
-	}
-
+	} 
+	
 	@Override
 	public View getChildView(int groupPosition, final int childPosition,
 			boolean isLastChild, View convertView, ViewGroup parent) {
 
 		final String childText = (String) getChild(groupPosition, childPosition);
-	/*	int id_res = 0;
+		int id_res = 0;
 		long group_id = getGroupId(groupPosition);
 		
 		if (group_id == 0) {
 			id_res = R.drawable.poi_sightseeing;
-		}
-		else if (group_id == 1) {
+		} else if (group_id == 1) {
 			id_res = R.drawable.poi_museum;
-		}
-		else if (group_id == 2) {
+		} else if (group_id == 2) {
 			id_res = R.drawable.poi_shopping;
-		}
-		else if (group_id == 3) {
+		} else if (group_id == 3) {
 			id_res = R.drawable.poi_eat;
-		}
-		else if (group_id == 4) {
+		} else if (group_id == 4) {
 			id_res = R.drawable.poi_cafe;
-		}
-		else if (group_id == 5) {
+		} else if (group_id == 5) {
 			id_res = R.drawable.poi_bar;
-
 		} else if (group_id == 6) {
 			id_res = R.drawable.poi_hidden;
-		}*/
+		}
 
 		if (convertView == null) {
 			LayoutInflater infalInflater = (LayoutInflater) this._context
 					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 			convertView = infalInflater.inflate(R.layout.list_item, null);
-		/*	ViewHolder holderImage = new ViewHolder(convertView);
-			holderImage.imageview.setImageResource(id_res);*/
+			ViewHolderListitem holderImage = new ViewHolderListitem(convertView);
+			holderImage.imageview.setImageResource(id_res);
 		}
 
 		TextView txtListChild = (TextView) convertView
@@ -79,6 +75,8 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
 		txtListChild.setText(childText);
 		return convertView;
 	}
+	
+	
 
 	@Override
 	public int getChildrenCount(int groupPosition) {
@@ -90,7 +88,7 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
 	public Object getGroup(int groupPosition) {
 		return this._listDataHeader.get(groupPosition);
 	}
-
+	
 	@Override
 	public int getGroupCount() {
 		return this._listDataHeader.size();
@@ -104,6 +102,7 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
 	@Override
 	public View getGroupView(int groupPosition, boolean isExpanded,
 			View convertView, ViewGroup parent) {
+		
 		String headerTitle = (String) getGroup(groupPosition);
 		int id_res = 0;
 
@@ -111,40 +110,47 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
 			id_res = R.drawable.poi_sightseeing;
 		} else if (headerTitle.equals("Museum, venue")) {
 			id_res = R.drawable.poi_museum;
-		}
-
-		else if (headerTitle.equals("Shopping")) {
+		} else if (headerTitle.equals("Shopping")) {
 			id_res = R.drawable.poi_shopping;
-		}
-
-		else if (headerTitle.equals("Eat, drink")) {
+		} else if (headerTitle.equals("Eat, drink")) {
 			id_res = R.drawable.poi_eat;
-		}
-
-		else if (headerTitle.equals("Café, tea room")) {
+		} else if (headerTitle.equals("Café, tea room")) {
 			id_res = R.drawable.poi_cafe;
-		}
-
-		else if (headerTitle.equals("Bar, club")) {
+		} else if (headerTitle.equals("Bar, club")) {
 			id_res = R.drawable.poi_bar;
 		} else if (headerTitle.equals("Hidden, chill out")) {
 			id_res = R.drawable.poi_hidden;
 		}
-
+		
 		if (convertView == null) {
 			LayoutInflater infalInflater = (LayoutInflater) this._context
 					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 			convertView = infalInflater.inflate(R.layout.list_group, null);
-			ViewHolder holder = new ViewHolder(convertView);
+			ViewHolderHeader holder = new ViewHolderHeader(convertView);
 			holder.imageview.setImageResource(id_res);
 		}
 
 		TextView lblListHeader = (TextView) convertView
 				.findViewById(R.id.lblListHeader);
-		// lblListHeader.setTypeface(null, Typeface.BOLD);
+		lblListHeader.setTypeface(null, Typeface.BOLD);
 		lblListHeader.setText(headerTitle);
 
 		return convertView;
+	}
+	
+	public class ViewHolderHeader {
+		public TextView text;
+		public ImageView imageview;
+		public ViewHolderHeader(View v) {
+			this.imageview = (ImageView) v.findViewById(R.id.headerimage);
+		}
+	}
+	public class ViewHolderListitem {
+		public TextView text;
+		public ImageView imageview;
+		public ViewHolderListitem(View v) {
+			this.imageview = (ImageView) v.findViewById(R.id.listimage);
+		}
 	}
 
 	@Override
