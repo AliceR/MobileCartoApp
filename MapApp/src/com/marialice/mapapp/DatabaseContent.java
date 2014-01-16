9872ab2182d263e9ba2e22b5ac1819c523ac55d3
AliceR <alice.ruehl@gmail.com>
2014-01-16 18:20:41 +0100

    database class

---
+++ MapApp/src/com/marialice/mapapp/DatabaseContent.java
@@ -0,0 +1,61 @@
+package com.marialice.mapapp;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.android.gms.maps.model.Marker;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class DatabaseContent extends Activity{
+	SQLiteDatabase db = null;
+	Cursor dbCursor;
+	DatabaseHelper dbHelper = new DatabaseHelper(this);
+	
+	
+	public final List<Marker> dbMarker = new ArrayList<Marker>();
+	
+	public final List<String> listSightseeing = new ArrayList<String>();
+	public final List<String> listMuseum = new ArrayList<String>();
+	public final List<String> listShopping = new ArrayList<String>();
+	public final List<String> listEat = new ArrayList<String>();
+	public final List<String> listCafe = new ArrayList<String>();
+	public final List<String> listBar = new ArrayList<String>();
+	public final List<String> listHidden = new ArrayList<String>();
+	
+	
+	public void queryDataFromDatabase(){
+		try {
+			dbHelper.createDataBase();
+		} catch (IOException ioe) {
+		}
+		try {
+			db = dbHelper.getDataBase();
+			dbCursor = db.rawQuery(
+					"SELECT * FROM cbpois;",
+					null);
+			dbCursor.moveToFirst();
+			
+			int idindex = dbCursor.getColumnIndex("id");
+			int latindex = dbCursor.getColumnIndex("lat");
+			int lonindex = dbCursor.getColumnIndex("lon");
+			int catindex = dbCursor.getColumnIndex("category");
+			int titleindex = dbCursor.getColumnIndex("title");
+			int descindex = dbCursor.getColumnIndex("description");
+			
+			while (!dbCursor.isAfterLast()) {
+				
+				
+				
+				
+			}
+		} finally {
+			if (db != null) {
+				dbHelper.close();
+			}
+		}
+	}
+}
