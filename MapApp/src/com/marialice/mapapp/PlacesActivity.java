ffc77470588b166fffffb1fc4f47ec220da71da0
AliceR <alice.ruehl@gmail.com>
2014-01-17 17:14:21 +0100

    connect PlacesActivity to Poi class

--- MapApp/src/com/marialice/mapapp/PlacesActivity.java
+++ MapApp/src/com/marialice/mapapp/PlacesActivity.java
@@ -1,16 +1,14 @@
 package com.marialice.mapapp;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,9 +17,7 @@ import android.widget.ExpandableListView.OnChildClickListener;
 
 public class PlacesActivity extends Activity {
 
-	SQLiteDatabase db = null;
-	Cursor dbCursor;
-	DatabaseHelper dbHelper = new DatabaseHelper(this);
+	DatabaseContent dbclass = new DatabaseContent();
 
 	ExpandableListAdapter listAdapter;
 	ExpandableListView expListView;
@@ -48,7 +44,7 @@ public class PlacesActivity extends Activity {
 	}
 
 	private void setupActionBar() {
-		// Defines the action bar
+		// Defines the action bar to go back/up
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 	}
 
@@ -68,14 +64,10 @@ public class PlacesActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
+
 	// Preparing the list data
 	private void prepareListData() {
-		try {
-			dbHelper.createDataBase();
-		} catch (IOException ioe) {
-		}
-		
+
 		listDataHeader = new ArrayList<String>();
 		listDataChild = new HashMap<String, List<String>>();
 
@@ -88,154 +80,46 @@ public class PlacesActivity extends Activity {
 		listDataHeader.add("Bar, club");
 		listDataHeader.add("Hidden, chill out");
 
-		// Adding sightseeing data
-		List<String> sightseeing = new ArrayList<String>();
-		try {
-			db = dbHelper.getDataBase();
-			dbCursor = db.rawQuery(
-					"SELECT title FROM cbpois WHERE category = 'sightseeing';",
-					null);
-			dbCursor.moveToFirst();
-			int titleindex = dbCursor.getColumnIndex("title");
-			while (!dbCursor.isAfterLast()) {
-				String title = dbCursor.getString(titleindex);
-				sightseeing.add(title);
-				dbCursor.moveToNext();
-			}
-		} finally {
-			if (db != null) {
-				dbHelper.close();
+		List<Poi> dbpois = dbclass.queryDataFromDatabase(this);
+
+		List<String> listSightseeing = new ArrayList<String>();
+		List<String> listMuseum = new ArrayList<String>();
+		List<String> listShopping = new ArrayList<String>();
+		List<String> listEat = new ArrayList<String>();
+		List<String> listCafe = new ArrayList<String>();
+		List<String> listBar = new ArrayList<String>();
+		List<String> listHidden = new ArrayList<String>();
+
+		for (int i = 0; i < dbpois.size(); i++) {
+			Poi poi = dbpois.get(i);
+			if (poi.getCategory().equals("sightseeing")) {
+				listSightseeing.add(poi.getTitle());
+			} else if (poi.getCategory().equals("museum")) {
+				listMuseum.add(poi.getTitle());
+			} else if (poi.getCategory().equals("shopping")) {
+				listShopping.add(poi.getTitle());
+			} else if (poi.getCategory().equals("eat")) {
+				listEat.add(poi.getTitle());
+			} else if (poi.getCategory().equals("cafe")) {
+				listCafe.add(poi.getTitle());
+			} else if (poi.getCategory().equals("bar")) {
+				listBar.add(poi.getTitle());
+			} else if (poi.getCategory().equals("hidden")) {
+				listHidden.add(poi.getTitle());
+			} else {
+				Log.v("Mary says:","WTF! there is an poi without category! " +
+						"...what a terrible failure...");
 			}
 		}
 
-		// Adding museum data
-		List<String> museum = new ArrayList<String>();
-		try {
-			db = dbHelper.getDataBase();
-			dbCursor = db
-					.rawQuery(
-							"SELECT title FROM cbpois WHERE category = 'museum';",
-							null);
-			dbCursor.moveToFirst();
-			int titleindex = dbCursor.getColumnIndex("title");
-			while (!dbCursor.isAfterLast()) {
-				String title = dbCursor.getString(titleindex);
-				museum.add(title);
-				dbCursor.moveToNext();
-			}
-		} finally {
-			if (db != null) {
-				dbHelper.close();
-			}
-		}
-
-		// Adding shopping data
-		List<String> shopping = new ArrayList<String>();
-		try {
-			db = dbHelper.getDataBase();
-			dbCursor = db.rawQuery(
-					"SELECT title FROM cbpois WHERE category = 'shopping';",
-					null);
-			dbCursor.moveToFirst();
-			int titleindex = dbCursor.getColumnIndex("title");
-			while (!dbCursor.isAfterLast()) {
-				String title = dbCursor.getString(titleindex);
-				shopping.add(title);
-				dbCursor.moveToNext();
-			}
-		} finally {
-			if (db != null) {
-				dbHelper.close();
-			}
-		}
-
-		// Adding eat data
-		List<String> eat = new ArrayList<String>();
-		try {
-			db = dbHelper.getDataBase();
-			dbCursor = db.rawQuery(
-					"SELECT title FROM cbpois WHERE category = 'eat';", null);
-			dbCursor.moveToFirst();
-			int titleindex = dbCursor.getColumnIndex("title");
-			while (!dbCursor.isAfterLast()) {
-				String title = dbCursor.getString(titleindex);
-				eat.add(title);
-				dbCursor.moveToNext();
-			}
-		} finally {
-			if (db != null) {
-				dbHelper.close();
-			}
-		}
-
-		// Adding cafe data
-		List<String> cafe = new ArrayList<String>();
-		try {
-			db = dbHelper.getDataBase();
-			dbCursor = db.rawQuery(
-					"SELECT title FROM cbpois WHERE category = 'cafe';", null);
-			dbCursor.moveToFirst();
-			int titleindex = dbCursor.getColumnIndex("title");
-			while (!dbCursor.isAfterLast()) {
-				String title = dbCursor.getString(titleindex);
-				cafe.add(title);
-				dbCursor.moveToNext();
-			}
-		} finally {
-			if (db != null) {
-				dbHelper.close();
-			}
-		}
-
-		// Adding bar data
-		List<String> bar = new ArrayList<String>();
-		try {
-			db = dbHelper.getDataBase();
-			dbCursor = db.rawQuery(
-					"SELECT title FROM cbpois WHERE category = 'bar';", null);
-			dbCursor.moveToFirst();
-			int titleindex = dbCursor.getColumnIndex("title");
-			while (!dbCursor.isAfterLast()) {
-				String title = dbCursor.getString(titleindex);
-				bar.add(title);
-				dbCursor.moveToNext();
-			}
-		} finally {
-			if (db != null) {
-				dbHelper.close();
-			}
-		}
-
-		// Adding hidden data
-		List<String> hidden = new ArrayList<String>();
-		try {
-
-			db = dbHelper.getDataBase();
-			dbCursor = db
-					.rawQuery(
-							"SELECT title FROM cbpois WHERE category = 'hidden';",
-							null);
-			dbCursor.moveToFirst();
-			int titleindex = dbCursor.getColumnIndex("title");
-			while (!dbCursor.isAfterLast()) {
-				String title = dbCursor.getString(titleindex);
-				hidden.add(title);
-				dbCursor.moveToNext();
-			}
-		} finally {
-			if (db != null) {
-				dbHelper.close();
-			}
-		}
-		
 		// Header, Child data
-		listDataChild.put(listDataHeader.get(0), sightseeing); 
-		listDataChild.put(listDataHeader.get(1), museum);
-		listDataChild.put(listDataHeader.get(2), shopping);
-		listDataChild.put(listDataHeader.get(3), eat);
-		listDataChild.put(listDataHeader.get(4), cafe);
-		listDataChild.put(listDataHeader.get(5), bar);
-		listDataChild.put(listDataHeader.get(6), hidden);
+		listDataChild.put(listDataHeader.get(0), listSightseeing);
+		listDataChild.put(listDataHeader.get(1), listMuseum);
+		listDataChild.put(listDataHeader.get(2), listShopping);
+		listDataChild.put(listDataHeader.get(3), listEat);
+		listDataChild.put(listDataHeader.get(4), listCafe);
+		listDataChild.put(listDataHeader.get(5), listBar);
+		listDataChild.put(listDataHeader.get(6), listHidden);
 
 		// Listview on child click listener
 		expListView.setOnChildClickListener(new OnChildClickListener() {
@@ -243,13 +127,15 @@ public class PlacesActivity extends Activity {
 			@Override
 			public boolean onChildClick(ExpandableListView parent, View v,
 					int groupPosition, int childPosition, long id) {
-				
-				Intent i = new Intent(getApplicationContext(), PlacesDescriptionActivity.class);
-				
-			    i.putExtra("listDataChild", listDataChild.get(listDataHeader.get(groupPosition))
-						.get(childPosition));
-			    startActivity(i);
-			    return false;
+
+				Intent listintent = new Intent(getApplicationContext(),
+						PlacesDescriptionActivity.class);
+
+				listintent.putExtra("listDataChild",
+						listDataChild.get(listDataHeader.get(groupPosition))
+								.get(childPosition));
+				startActivity(listintent);
+				return false;
 			}
 		});
 	}
