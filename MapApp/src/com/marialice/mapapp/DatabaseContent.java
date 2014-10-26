7acc1993b2e8b64246849e192428323bf36c8e33
MariDani <danielova.mariana@gmail.com>
2014-10-26 07:45:59 +0100

    walk water db access

--- MapApp/src/com/marialice/mapapp/DatabaseContent.java
+++ MapApp/src/com/marialice/mapapp/DatabaseContent.java
@@ -153,6 +153,129 @@ public class DatabaseContent extends Activity {
 		return dbmarkers;
 	}
 	
+	public List<WalkWater> queryWalksWaterFromDatabase(Context context) {
+
+		SQLiteDatabase db = null;
+		Cursor dbCursor;
+		DatabaseHelper dbHelper = new DatabaseHelper(context);
+
+		List<WalkWater> dbwalkwaternodes = new ArrayList<WalkWater>();
+
+		try {
+			dbHelper.createDataBase();
+		} catch (IOException ioe) {
+		}
+		try {
+			db = dbHelper.getDataBase();
+			dbCursor = db.rawQuery("SELECT * FROM walkwater;", null);
+			dbCursor.moveToFirst();
+
+			int latindex = dbCursor.getColumnIndex("latitude");
+			int lonindex = dbCursor.getColumnIndex("longitude");
+			
+
+			while (!dbCursor.isAfterLast()) {
+
+				Double lat = dbCursor.getDouble(latindex);
+				Double lon = dbCursor.getDouble(lonindex);
+
+				WalkWater walkwaternode = new WalkWater();
+
+				walkwaternode.setLat(lat);
+				walkwaternode.setLon(lon);
+
+				dbwalkwaternodes.add(walkwaternode);
+
+				dbCursor.moveToNext();
+
+			}
+		} finally {
+			if (db != null) {
+				dbHelper.close();
+			}
+		}
+		return dbwalkwaternodes;
+	}
+	
+	
+	
+	public List<WalkWaterPoi> queryWalksWaterPoiFromDatabase(Context context) {
+
+		SQLiteDatabase db = null;
+		Cursor dbCursor;
+		DatabaseHelper dbHelper = new DatabaseHelper(context);
+
+		List<WalkWaterPoi> dbwalkwaterpois = new ArrayList<WalkWaterPoi>();
+
+		try {
+			dbHelper.createDataBase();
+		} catch (IOException ioe) {
+		}
+		try {
+			db = dbHelper.getDataBase();
+			dbCursor = db.rawQuery("SELECT * FROM walkwater_pois;", null);
+			dbCursor.moveToFirst();
+			
+			int idindex = dbCursor.getColumnIndex("id");
+			int latindex = dbCursor.getColumnIndex("Lat");
+			int lonindex = dbCursor.getColumnIndex("Lon");
+			int titleindex = dbCursor.getColumnIndex("title");
+			int descindex = dbCursor.getColumnIndex("popis");
+			int nameindex = dbCursor.getColumnIndex("name");
+			int iconindex = dbCursor.getColumnIndex("icon");
+			int wifiindex = dbCursor.getColumnIndex("wifi");
+			int terraceindex = dbCursor.getColumnIndex("terrace");
+			int sundaysindex = dbCursor.getColumnIndex("sundays");
+			int calmplaceindex = dbCursor.getColumnIndex("calmplace");
+			int nonsmokingindex = dbCursor.getColumnIndex("nonsmoking");
+			int touristclassicindex = dbCursor.getColumnIndex("touristclassic");
+
+			while (!dbCursor.isAfterLast()) {
+
+				int id = dbCursor.getInt(idindex);
+				Double lat = dbCursor.getDouble(latindex);
+				Double lon = dbCursor.getDouble(lonindex);
+				String title = dbCursor.getString(titleindex);
+				String description = dbCursor.getString(descindex);
+				String name = dbCursor.getString(nameindex);
+				String icon = dbCursor.getString(iconindex);
+				Boolean wifi = dbCursor.getInt(wifiindex) > 0;
+				Boolean terrace = dbCursor.getInt(terraceindex) > 0;
+				Boolean sundays = dbCursor.getInt(sundaysindex) > 0;
+				Boolean calmplace = dbCursor.getInt(calmplaceindex) > 0;
+				Boolean nonsmoking = dbCursor.getInt(nonsmokingindex) > 0;
+				Boolean touristclassic = dbCursor.getInt(touristclassicindex) > 0;
+
+				WalkWaterPoi walkwaterpoi = new WalkWaterPoi();
+
+				walkwaterpoi.setId(id);
+				walkwaterpoi.setLat(lat);
+				walkwaterpoi.setLon(lon);
+				walkwaterpoi.setTitle(title);
+				walkwaterpoi.setDescription(description);
+				walkwaterpoi.setName(name);
+				walkwaterpoi.setIcon(icon);
+				walkwaterpoi.setWifi(wifi);
+				walkwaterpoi.setTerrace(terrace);
+				walkwaterpoi.setSundays(sundays);
+				walkwaterpoi.setCalmplace(calmplace);
+				walkwaterpoi.setNonsmoking(nonsmoking);
+				walkwaterpoi.setTouristclassic(touristclassic);
+
+				dbwalkwaterpois.add(walkwaterpoi);
+
+				dbCursor.moveToNext();
+
+			}
+		} finally {
+			if (db != null) {
+				dbHelper.close();
+			}
+		}
+		return dbwalkwaterpois;
+	}
+	
+	
 	public List<Hint> queryHintsFromDatabase(Context context) {
 
 		SQLiteDatabase db = null;
