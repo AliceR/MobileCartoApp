eb913131a360805e954030923fc3f14547d01929
MariDani <danielova.mariana@gmail.com>
2014-10-27 23:53:10 +0100

    add walk beer and walk sights

---
+++ MapApp/src/com/marialice/mapapp/WalkBeer.java
@@ -0,0 +1,33 @@
+package com.marialice.mapapp;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class WalkBeer {
+
+	private Double lat;
+	private Double lon;
+	
+
+	public Double getLat() {
+		return lat;
+	}
+
+	public void setLat(Double lat) {
+		this.lat = lat;
+	}
+
+	public Double getLon() {
+		return lon;
+	}
+	
+	public LatLng getLatLng() {
+		LatLng position = new LatLng(lat, lon);
+		return position;
+	}
+
+	public void setLon(Double lon) {
+		this.lon = lon;
+	}
+
+
+}
